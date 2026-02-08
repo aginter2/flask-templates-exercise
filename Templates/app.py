d578cb35d5d79f5b7e79a9e07961b58be471e576
aginter2 <aginter2@asu.edu>
2026-02-08 14:54:21 -0700

    Add contact page with basic form

--- Templates/app.py
+++ Templates/app.py
@@ -11,6 +11,10 @@ def home():
 def about():
     return render_template("about.html", page_title="About Us")
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", page_title="Contact")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
